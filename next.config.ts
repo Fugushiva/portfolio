7f7ae0a231d12b81d2f8923a1c2f79ac15a1b50f
Fugushiva <jeromedelodder90@gmail.com>
2026-04-28 13:00:19 +0700

    correct some issues

--- next.config.ts
+++ next.config.ts
@@ -8,9 +8,7 @@ const config: NextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
   },
-  experimental: {
-    optimizePackageImports: ['framer-motion', 'gsap'],
-  },
+
 }
 
 export default withNextIntl(config)
